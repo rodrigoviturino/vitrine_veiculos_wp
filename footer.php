0491788e0c07707484c361f1650c6343a9c213aa
Rodrigo Viturino <viturino.souza1@gmail.com>
2019-04-30 01:59:26 -0300

    Inserindo sessao estatica onde a API vai alimentar, de acordo com a localidade selecionada, irá trazer lojas proximas

--- footer.php
+++ footer.php
@@ -1,21 +1,31 @@
 
-<footer id="footer">
+<footer class="footer">
     <div class="container">
 
     <div class="row">
     
-        <div class="col-md-8">
+        <!-- <div class="col-md-8">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29248.913740764674!2d-46.73430095167264!3d-23.600236342069618!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce56dc83c108ab%3A0x77a33d0ae82abc21!2sMorumbi%2C+S%C3%A3o+Paulo+-+SP!5e0!3m2!1spt-BR!2sbr!4v1556599289281!5m2!1spt-BR!2sbr" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
-        </div>
-        <div class="col-md-4">
-            <ul>
+        </div> -->
+        <div class="col-md-4 footer__lojas">
                 <h3>Lojas Próximas</h3>
-                <li></li>
+            <ul>
+                <li>São Paulo - Bairro: Saúde - Loja 01</li>
+                <li>São Paulo - Bairro: Guaianases - Shopping</li>
+                <li>São Paulo - Bairro: Aricanduva - Shopping</li>
+                <li>São Paulo - Bairro: Tatuapé - Loja 03</li>
+            </ul>
+        </div>
+        <div class="col-md-4 footer__lojas">
+            <h3>Lojas Próximas</h3>
+            <ul>
+                <li>São Paulo - Bairro: Saúde - Loja 01</li>
+                <li>São Paulo - Bairro: Guaianases - Shopping</li>
+                <li>São Paulo - Bairro: Aricanduva - Shopping</li>
+                <li>São Paulo - Bairro: Tatuapé - Loja 03</li>
             </ul>
         </div>
 
-        <div class="col-md-9">
-        </div>
         <div class="col-md-3">
             <h3>Entre em Contato</h3>
             <form action="#">
